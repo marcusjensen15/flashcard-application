4d4bb1d4a4f5ea6ea68adce5ec654f8c06a07550
Marcus Jensen <marcusjensen1515@gmail.com>
2020-06-08 11:07:08 -0700

    make app.js stateful component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import './App.css';
 import Card from './Card/Card';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      cards: [
+        {id: 1, eng: "English", spanish: "Spanish"},
+        {id: 2, eng: "English2", spanish: "Spanish2"}
+      ],
+      currentCard: {}
+    }
+  }
+
+  componentWillMount(){
+    const currentCards = this.state.cards;
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards)
+    })
+  }
+
   render(){
   return (
     <div className="App">
