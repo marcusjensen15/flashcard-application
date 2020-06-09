4205a27c2f0331c77ef8ec2968603194d4045ddd
Marcus Jensen <marcusjensen1515@gmail.com>
2020-06-09 15:10:13 -0700

    installed routing in the application. App will eventually need to be sectioned out into its own component

---
+++ src/AddCard/AddCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './AddCard.css'
+
+
+
+export default function AddCard(){
+
+
+
+return(
+<div className="instructionsContainer">
+  <p>This is the AddCard component</p>
+</div>
+)
+}
