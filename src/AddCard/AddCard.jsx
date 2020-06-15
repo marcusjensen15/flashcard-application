b4d6e5b3a0b79dead5a2816f29520cb1e4befaea
Marcus Jensen <marcusjensen1515@gmail.com>
2020-06-15 09:33:14 -0700

    add form submission and button to add a card page

--- src/AddCard/AddCard.jsx
+++ src/AddCard/AddCard.jsx
@@ -9,12 +9,16 @@ export default function AddCard(){
 
 return(
 <div className="AddCardContainer">
-  <div className="addCardTitle">Add a Card</div>
-  <div className="inputContainer">
-    <textarea className="cardFront" type="text" placeholder="Card Front"/>
-    <textarea className="cardBack" type="text" placeholder="Card Back"/>
-    <input className="deckName" type="text" placeholder="Deck Name"/>
-  </div>
+  <form>
+    <div className="addCardTitle">Add a Card</div>
+    <div className="inputContainer">
+      <textarea className="cardFront" type="text" placeholder="Card Front"/>
+      <textarea className="cardBack" type="text" placeholder="Card Back"/>
+      <input className="deckName" type="text" placeholder="Deck Name"/>
+      <div  className="submitButtonStyle">  <button className="buttonSize" type='submit'>Add Post</button> </div>
+
+    </div>
+  </form>
 </div>
 )
 }
