ed13bec925c18982b925e8ac0ce5f48d4aa0bb10
Marcus Jensen <marcusjensen1515@gmail.com>
2020-06-16 10:56:32 -0700

    begin to write seperate component for training mode

--- src/index.js
+++ src/index.js
@@ -6,24 +6,22 @@ import AddCard from './AddCard/AddCard';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import { DB_CONFIG } from './Config/Firebase/db_config';
+import { HashRouter } from 'react-router-dom';
 
 const firebase = require('firebase');
 firebase.initializeApp(DB_CONFIG);
 
-const routing = (
-  <Router>
-    <div id='routing-container'>
-      <Route path='/addcard' component={AddCard}></Route>
-        <Route path='/home' component={App}></Route>
+// const routing = (
+//   <Router>
+//     <div id='routing-container'>
+//       <Route path='/addcard' component={AddCard}></Route>
+//       <Route path='/home' component={App}></Route>
+//
+//     </div>
+//   </Router>
+// );
 
-    </div>
-  </Router>
-);
-
-ReactDOM.render(
-  routing,
-  document.getElementById('root')
-);
+ReactDOM.render(<HashRouter><App/></HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
