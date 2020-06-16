86a9d051bb417def0e1606cc11a5a8596c091342
Marcus Jensen <marcusjensen1515@gmail.com>
2020-06-16 10:46:07 -0700

    begin to build form to communicate with api

--- src/AddCard/AddCard.jsx
+++ src/AddCard/AddCard.jsx
@@ -3,7 +3,11 @@ import './AddCard.css'
 
 
 
-export default function AddCard(){
+export default function AddCard(props){
+
+  let _cardFront = null;
+  let _cardBack= null;
+  let _deckName = null;
 
 
   function addNewCard(event) {
@@ -18,9 +22,24 @@ return(
   <form className="formContainer" onSubmit={addNewCard}>
     <div className="addCardTitle">Add a Card</div>
     <div className="inputContainer">
-      <textarea className="cardFront" type="text" placeholder="Card Front"/>
-      <textarea className="cardBack" type="text" placeholder="Card Back"/>
-      <input className="deckName" type="text" placeholder="Deck Name"/>
+      <textarea className="cardFront"
+        type="text"
+        placeholder="Card Front"
+        ref={(input) => {_cardFront = input;}}/>
+
+
+      <textarea className="cardBack"
+        type="text"
+        placeholder="Card Back"
+        ref={(input) => {_cardBack = input;}}/>
+
+
+      <input className="deckName"
+        type="text"
+        placeholder="Deck Name"
+        ref={(input) => {_deckName = input;}}/>
+
+
       <div  className="submitButtonStyle">  <button className="buttonSize" type='submit'>Add Card</button> </div>
 
     </div>
