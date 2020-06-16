456fe96167dd5f6bd2494b834ab70c7558a0243d
Marcus Jensen <marcusjensen1515@gmail.com>
2020-06-16 11:40:23 -0700

    add basic home component

---
+++ src/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/resumeStyle.css';
+
+
+export default function Resume(){
+
+
+  return(
+    <>
+      <Link to='/train'> Train </Link>
+      <Link to='/addCard'> addCards </Link>
+
+    </>
+  );
+}
