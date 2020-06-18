6e12c0583ba888b7936c2854327670c195ccd2df
Marcus Jensen <marcusjensen1515@gmail.com>
2020-06-18 13:35:27 -0700

    componentWillMount sucessfully setting cards state

--- src/App.js
+++ src/App.js
@@ -25,11 +25,14 @@ class App extends Component{
     }
   }
 
+//set 'cards' state inside of componentWillMount
 
-  componentWillMount(){
-    axios.get('http://localhost:4000/cards').then(resp => {
-      console.log(resp.data)
+  componentWillMount = async () => {
+    let callCards = await axios.get('http://localhost:4000/cards').then(resp => {
+       return resp.data;
     })
+    await this.setState({cards: callCards});
+    console.log(this.state.cards);
   }
 
 addNewCard = (newCard) => {
