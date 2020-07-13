e462020b8cd7f05cfafc6556b87adef5901c1374
Marcus Jensen <marcusjensen1515@gmail.com>
2020-07-13 10:04:55 -0700

    signup component now has input fields

--- src/Signup/Signup.jsx
+++ src/Signup/Signup.jsx
@@ -7,19 +7,58 @@ class Signup extends Component{
   constructor(props){
     super(props);
     this.state = {
-      email: null,
-      confirmEmail: null,
-      password: null,
-      confirmPassword: null
+      email: "",
+      confirmEmail: "",
+      password: "",
+      confirmPassword: ""
     }
   }
+
+  handleSubmit = (event) => {
+    alert('This is the state: ' + this.state.email + this.state.confirmEmail + this.state.password + this.stateconfirmPassword);
+    event.preventDefault();
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      email: event.target.email,
+      confirmEmail: event.target.confirmEmail,
+      password: event.target.password,
+      confirmPassword: event.target.confirmPassword
+      });
+  };
 
 
   render(props){
     return(
       <div className="signupContainer">
         <Link className="linkText" to='/'> Home </Link>
-        <p >Signup Page</p>
+
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Email:
+            <input type="text" value={this.state.email} onChange={this.handleChange} />
+          </label>
+
+          <label>
+            Confirm Email:
+          <input type="text" value={this.state.confirmEmail} onChange={this.handleChange} />
+          </label>
+
+          <label>
+            Password:
+            <input type="text" value={this.state.password} onChange={this.handleChange} />
+          </label>
+
+          <label>
+            Confirm Password:
+            <input type="text" value={this.state.confirmPassword} onChange={this.handleChange} />
+          </label>
+
+
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     )
   }
