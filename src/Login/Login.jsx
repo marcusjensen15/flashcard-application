7dd92e7f3f553a59982a4093686f7601df57abd3
Marcus Jensen <marcusjensen1515@gmail.com>
2020-07-13 09:32:03 -0700

    add ability to navigate back to home when user is inside of Login or Signup view

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Login.css';
+import {Link} from 'react-router-dom';
 
 
 class Login extends Component{
@@ -11,6 +12,7 @@ class Login extends Component{
   render(props){
     return(
       <div className="loginContainer">
+        <Link className="linkText" to='/'> Home </Link>
         <p >Login Page</p>
       </div>
     )
--- src/Signup/Signup.jsx
+++ src/Signup/Signup.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 
 
@@ -11,6 +12,7 @@ class Signup extends Component{
   render(props){
     return(
       <div className="signupContainer">
+        <Link className="linkText" to='/'> Home </Link>
         <p >Signup Page</p>
       </div>
     )
