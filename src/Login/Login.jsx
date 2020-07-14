332d06ee80f65c5ffea98245ab1fd0f91f164609
Marcus Jensen <marcusjensen1515@gmail.com>
2020-07-14 09:07:00 -0700

    copy signup template over to login

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,22 +1,61 @@
 import React, { Component } from 'react';
-import './Login.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
-class Login extends Component{
-  constructor(props){
-    super(props);
-  }
+
+export default function Login(){
+
+  let email = null;
+  let confirmEmail = null;
+  let password = null;
+  let confirmPassword = null;
 
 
-  render(props){
+  function handleSubmit(event){
+    event.preventDefault();
+    console.log({
+      email: email.value,
+      confirmEmail: confirmEmail.value,
+      password: password.value,
+      confirmPassword: confirmPassword.value
+      });
+
+      email.value = '';
+      confirmEmail.value = '';
+      password.value = '';
+      confirmPassword.value = '';
+  };
+
+
     return(
       <div className="loginContainer">
         <Link className="linkText" to='/'> Home </Link>
-        <p >Login Page</p>
+
+
+        <form onSubmit={handleSubmit}>
+          <label>
+            Email:
+            <input type="text" ref={(input) => {email = input;}}/>
+          </label>
+
+          <label>
+            Confirm Email:
+          <input type="text" ref={(input) => {confirmEmail = input;}}/>
+          </label>
+
+          <label>
+            Password:
+            <input type="text" ref={(input) => {password = input;}}/>
+          </label>
+
+          <label>
+            Confirm Password:
+            <input type="text" ref={(input) => {confirmPassword = input;}}/>
+          </label>
+
+
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     )
-  }
 }
-
-export default Login
